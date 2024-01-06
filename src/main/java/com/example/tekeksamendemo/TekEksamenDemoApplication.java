2156d0ce19134775a6aeeabda0e5de91d5a07e33
mikk39k1 <mikk39k1@stud.kea.dk>
2024-01-06 14:30:12 +0100

    first commit

---
+++ src/main/java/com/example/tekeksamendemo/TekEksamenDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tekeksamendemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekEksamenDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TekEksamenDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tekeksamendemo/TekEksamenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tekeksamendemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TekEksamenDemoApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
